582df6eb64b6aaabd17ad5f90631f3f173264693
Lucas Silveira <101433725+LuskasWalker@users.noreply.github.com>
2025-04-12 19:13:23 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-echo "Iniciando servidor Minecraft..."
+
+# Baixa o JAR se não existir ainda
+if [ ! -f server.jar ]; then
+    echo "Baixando server.jar..."
+    curl -o server.jar https://api.papermc.io/v2/projects/paper/versions/1.20.1/builds/132/downloads/paper-1.20.1-132.jar
+fi
+
+# Inicia o servidor
+echo "Iniciando servidor..."
 java -Xms512M -Xmx1G -jar server.jar nogui
